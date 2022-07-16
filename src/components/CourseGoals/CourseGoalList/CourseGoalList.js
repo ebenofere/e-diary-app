d665859769c0c65ee41323353c6ac0766f4eebf8
ebenofere <eofere@gmail.com>
2022-07-16 08:54:56 +0100

    work in progress

--- src/components/CourseGoals/CourseGoalList/CourseGoalList.js
+++ src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-import CourseGoalItem from "../CourseGoalItem/CourseGoalItem/CourseGoalItem";
+import CourseGoalItem from "../CourseGoalItem/CourseGoalItem";
 
 const CourseGoalList = (props) => {
+  console.log(props, "CourseGoalList");
+
   return (
     <ul className="goal-list">
-      {props.items.map((goal) => {
+      {props.items.map((goal) => (
         <CourseGoalItem key={goal.id} id={goal.id}>
           {goal.text}
-        </CourseGoalItem>;
-      })}
+        </CourseGoalItem>
+      ))}
     </ul>
   );
 };
